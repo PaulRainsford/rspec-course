51d2a1303f9ad7ae4d2f42a2a4384622ed7bc281
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-30 21:10:35 +0100

    Added hook and instance variable for card and passing tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,5 @@
 class Card
   attr_reader :rank, :suit
-
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
@@ -8,14 +7,17 @@ class Card
 end
 
 RSpec.describe Card do
-    it 'has a rank' do
-      card = Card.new('Ace', 'Spades')
-      expect(card.rank).to eq('Ace')
+  before do
+    @card = Card.new('Ace', 'Spades')
+  end
+    
+  
+  it 'has a rank' do
+      expect(@card.rank).to eq('Ace')
     end
 
     it 'has a suit' do
-      card = Card.new('Ace', 'Spades')
-      expect(card.suit).to eq('Spades')
+      expect(@card.suit).to eq('Spades')
     end
 
 end

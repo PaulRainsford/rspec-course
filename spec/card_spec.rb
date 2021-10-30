144d6d6af247400a12d19312341611a33078242c
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-30 22:15:07 +0100

    Added a custom error message

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -9,6 +9,8 @@ end
 RSpec.describe Card do
   let(:card) { Card.new('Ace', 'Spades') }
     
+  
+  
   it 'has a rank and that rank can change' do
       expect(card.rank).to eq('Ace')
       card.rank = 'Queen'
@@ -18,4 +20,10 @@ RSpec.describe Card do
     it 'has a suit' do
       expect(card.suit).to eq('Spades')
     end
-end
+
+    it 'has a custom error message' do
+      card.suit = 'Nonsense'
+      comparison = 'Spades'
+      expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
+    end
+  end
